adc162245bfbd50acc9ee9dab05d2a21b4d9653b
kyjame01 <kyjame01@wsc.edu>
2025-09-29 14:48:00 -0500

    Prev. Updated CounterList and now i updated CounterCard

--- src/components/CounterCard.jsx
+++ src/components/CounterCard.jsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
-
-export default function CounterCard({ name }) { 
-  const [count, setCount] = useState(0); // state variable 
- 
+export default function CounterCard({ id, name, value, onUpdate, onReset, onDelete }) {  
   return ( 
     <div style={{ textAlign: "center", marginTop: "3rem", fontFamily: "Arial" }}> 
-        <h1>{name}</h1> 
-        <h2>Count: {count}</h2>
-        <button onClick={() => setCount(count + 1)}>Increment</button> 
-        <button onClick={() => setCount(count - 1)}>Decrement</button> 
-        <button onClick={() => setCount(0)}>Reset</button>
-        {count < 0 && <p style={{ color: "red" }}>Careful! Negative count!</p>}
+      <h1>{name}</h1> 
+      <h2>Count: {value}</h2>
+      <button onClick={() => onUpdate(id, 1)}>Increment</button> 
+      <button onClick={() => onUpdate(id, -1)}>Decrement</button> 
+      <button onClick={() => onReset(id)}>Reset</button>
+      <button onClick={() => onDelete(id)} style={{ color: "red" }}>Delete</button>
+      {value < 0 && <p style={{ color: "red" }}>Careful! Negative count!</p>}
     </div> 
   ); 
-} 
+}
